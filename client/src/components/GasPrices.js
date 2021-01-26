a12c610ba5ac67ff1cf8915ab7f606d3308d8abe
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-26 13:18:15 -0500

    lecture work-in-progress

--- client/src/components/GasPrices.js
+++ client/src/components/GasPrices.js
@@ -19,7 +19,9 @@ class GasPrices extends React.Component {
       }
     })
       .then(res=> {
-        console.log(res);
+        this.setState({
+          gasPrices: res.data.data
+        })
       })
       .catch(err=>{
         console.log(err.response.data.error);
