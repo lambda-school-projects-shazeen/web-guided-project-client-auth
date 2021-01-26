fd0e3305905547f5f23f8d9a56e903d498cb5730
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-26 14:01:51 -0500

    lecture work-in-progress

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+/*
+1. Wrap the a plain Route component and pass in the same props.
+2. Check to see if we are logged in, and if so, render component
+3. If the user is not logged in, we redirect to login
+*/
+
+
+const PrivateRoute = (props) =>  {
+
+};
