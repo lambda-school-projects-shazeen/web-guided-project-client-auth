f4880dda459e9d898874f17aaebc7020d6d0da4d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-26 13:26:57 -0500

    lecture work-in-progress

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       }
     })
       .then(res=>{
-        console.log(res);
+        localStorage.removeItem('token');
       })
       .catch(err=>{
         console.log(err);
